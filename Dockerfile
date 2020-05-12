d64d386de8f553e5b67a29c1513a8b7d33d52d8b
Your Name <you@example.com>
2020-05-12 17:44:46 +0800

    替换为ubuntu 16.04

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,9 @@
-FROM gcc:10
+FROM ubuntu:16.04
 
+COPY sources.list /etc/apt/sources.list
+RUN apt-get install -y apt-transport-https
 COPY entrypoint.sh /root/docker_entrypoint.sh
 RUN chmod 755 /root/docker_entrypoint.sh
-VOLUME ["/frida_out"]
-ENTRYPOINT ["/root/docker_entrypoint.sh"]
+WORKDIR /root
+# VOLUME ["/frida_out"]
+# ENTRYPOINT ["/root/docker_entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 cd /root \
-&& mv /etc/apt/sources.list /etc/apt/sources.list.back \
-&& echo "deb https://mirrors.aliyun.com/debian  stable main contrib non-free"> /etc/apt/sources.list \
-&& echo "deb https://mirrors.aliyun.com/debian  stable-updates main contrib non-free" >> /etc/apt/sources.list \
 && apt-get update \
+&& apt-get install -y python-software-properties software-properties-common \
+&& add-apt-repository -y ppa:ubuntu-toolchain-r/test \
+&& apt-get update \
+&& apt-get install -y build-essential gcc-multilib lib32stdc++-5-dev python-dev python3-dev git gcc-9 g++-9
 && wget -O android-ndk-r21b.zip https://dl.google.com/android/repository/android-ndk-r21b-linux-x86_64.zip \
 && unzip android-ndk-r21b.zip \
 && rm -f android-ndk-r21b.zip \
@@ -13,11 +14,9 @@ cd /root \
 && update-alternatives --install /usr/bin/python python /usr/bin/python3.7 30 \
 && apt-get install -y git flex bison build-essential gcc-multilib lib32stdc++-5-dev \
 && curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash - \
-&& apt-get install -y nodejs \
+&& apt-get install -y nodejs npm \
 && git clone --recurse-submodules https://github.com/frida/frida \
 && cd frida \
-&& make build/frida-android-arm/lib/pkgconfig/frida-core-1.0.pc \
-&& make build/frida-android-arm64/lib/pkgconfig/frida-core-1.0.pc \
-&& DESTDIR=/frida_out
+&& DESTDIR=/frida_out \
 && make build/frida-android-arm/lib/pkgconfig/frida-core-1.0.pc \
 && make build/frida-android-arm64/lib/pkgconfig/frida-core-1.0.pc
