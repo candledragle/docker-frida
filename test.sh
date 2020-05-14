8d477fd5d951bd560bb101de937f7fe81fe7b3a3
sym <s_abcabc@126.com>
2020-05-15 06:11:42 +0800

    fixbug

--- test.sh
+++ test.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-touch /frida_out/test.txt
-cat /proc/version > /frida_out/test.txt
+cd /root \
+&& apt-get update \
+&& apt-get install -y python-software-properties software-properties-common \
+&& add-apt-repository -y ppa:ubuntu-toolchain-r/test \
+&& apt-get update \
+&& apt-get install -y build-essential gcc-multilib lib32stdc++-5-dev python-dev python3-dev git gcc-9 g++-9 \
+flex bison curl wget unzip libterm-readkey-perl \
+&& update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 20 \
+&& update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-5 20 \
+&& update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 50 \
+&& update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-9 50 \
+&& ls /usr/bin/python* \
+&& curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash - \
+&& apt-get install -y nodejs \
+&& apt-get install -y npm \
